544b661dab3bcfa7df88b9569da2a84f68fe1447
mixanBit <65313661+mixanBit@users.noreply.github.com>
2021-05-03 14:05:41 +0300

    canvas_2

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 let canvas = document.getElementById('canvas')
     ctx    = canvas.getContext('2d')
-
-canvas.width = window.innerWidth; // берёт всю ширину
-canvas.height = window.innerHeight; // берёт всю высоту
-
+    canvas.width = window.innerWidth; // берёт всю ширину
+    canvas.height = window.innerHeight; // берёт всю высоту
 
 let x = 50
 
@@ -11,13 +9,24 @@ let x = 50
 // stroke (берёт border)
 // line (добавляет ширину border)
 
+
 ctx.fillStyle = 'pink'
 
+
 // (----круг----)
-ctx.arc(canvas.width / 2, canvas.height / 2, 100, 0, Math.PI * 2)
-ctx.fill()
+// ctx.arc(canvas.width / 2, canvas.height / 2, 100, 0, Math.PI * 2)
+// ctx.fill()
+
 
 // (----треугольник----)
+// ctx.beginPath()
+// ctx.moveTo(50, 50) // расположение
+// ctx.lineTo(25, 100) // сдвиг
+// ctx.lineTo(75, 100) // сдвиг
+// // ctx.lineTo(50, 50) // сдвиг
+// ctx.closePath()
+// ctx.stroke()
+
 
 //(----анимация----)
 // setInterval(() => {
